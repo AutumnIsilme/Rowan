6ff6810bd93bc0d9b36b19d74764a2d968b3569d
Doctor5555 <5555thedoctor@gmail.com>
2020-06-08 16:54:35 +0100

    Add timing to main and use token_type_names
    Also added stuff to the test file

--- Compiler/src/main.cpp
+++ Compiler/src/main.cpp
@@ -1,21 +1,26 @@
 #include <cstdio>
 #include <fstream>
+
 #include "Lexer.h"
+#include "Timer.h"
 
 int main(int argc, char **argv) {
     printf("Hello, World!\n");
 
-    std::string file_contents;
+    auto timer2 = Timer();
+    auto timer = Timer();
+
+    std::string contents;
 
     {
-    std::ifstream file("test.rw", std::ios::in | std::ios::binary);
-    if (file.is_open())
+    std::ifstream in("test.rw", std::ios::in | std::ios::binary);
+    if (in)
     {
-        file.seekg(0, std::ios::end);
-        file_contents.resize(file.tellg());
-        file.seekg(0, std::ios::beg);
-        file.read(&file_contents[0], file_contents.size());
-        file.close();
+        in.seekg(0, std::ios::end);
+        contents.resize(in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&contents[0], contents.size());
+        in.close();
     }
     else
     {
@@ -25,11 +30,21 @@ int main(int argc, char **argv) {
     }
     }
 
+    printf("Read file: %f\n", timer.elapsed());
+    timer.reset();
+
     auto lexer = Lexer();
-    lexer.make_tokens(file_contents);
+    lexer.make_tokens(contents);
+
+    printf("Make tokens: %f\n", timer.elapsed());
+    timer.reset();
+
     printf("%d\n", lexer.tokens->size());
     for (auto token : *lexer.tokens) {
-        printf("[%d: %s]\n", token.type, token.contents.c_str());
+        printf("[%s: %s]\n", token_type_names[(u16)token.type], token.contents.c_str());
     }
+
+    printf("Output tokens: %f\n", timer.elapsed());
+    printf("Total: %f\n", timer2.elapsed());
     return 0;
 }
