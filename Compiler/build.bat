8b872fc9e84f8ef03dcc50098d10a4de2758f724
Doctor5555 <5555thedoctor@gmail.com>
2020-06-08 16:54:57 +0100

    Added echo in build.bat to tell which type of build is being done

--- Compiler/build.bat
+++ Compiler/build.bat
@@ -6,9 +6,15 @@ pushd build
 if "%~1"=="-d" goto :debug
 if "%~1"=="-r" goto :release
 :debug
+echo. 
+echo ------------ Building debug ------------
+echo. 
 cl  /Fe"rwc" -Zi ../src/main.cpp ../src/Lexer.cpp
 goto :end
 :release
+echo. 
+echo ------------ Building release ------------
+echo. 
 cl  /Fe"rwc" ../src/main.cpp ../src/Lexer.cpp
 :end
 popd
