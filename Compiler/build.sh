883916976221ceda6021883742c2951538b0245b
Doctor5555 <5555thedoctor@gmail.com>
2020-06-12 17:11:25 +0100

    Reworked build.sh

--- Compiler/build.sh
+++ Compiler/build.sh
@@ -1,5 +1,4 @@
-helpfunc()
-{
+helpfunc() {
     echo
     echo "Usage: $0 [OPTIONS]"
     echo -e "\t-r/d Release mode or Debug mode build"
@@ -8,29 +7,36 @@ helpfunc()
     exit 1
 }
 
+function build_print {
+    echo
+    echo ------------ Building $1 $2 ------------
+    echo
+}
+
 while getopts "rdh" opt
 do
     case "$opt" in
-        r ) parameterR=1 ;;
-        d ) parameterR=0 ;;
+        r ) opt_lvl=-O3 lvl_name="release" ;;
+        d ) opt_lvl=-O0 lvl_name="debug" ;;
         h ) helpfunc ;;
         ? ) helpfunc ;;
     esac
 done
 
+if [[ "$lvl_name" != "release" ]]; then
+    lvl_name="debug"
+fi
+
 rm -rf build
 mkdir build
 
-if [ $parameterR -eq 0 ]; then
-    echo
-    echo ------------ Building debug ------------
-    echo
-    clang++ -O0 -o build/rwc.out src/main.cpp src/Lexer.cpp
-    exit $?
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    build_print LINUX $lvl_name
+    clang++ $opt_lvl -std=c++17 -o build/rwc.out src/main.cpp src/Lexer.cpp
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    build_print MACOS $lvl_name
+    clang++ $opt_lvl -std=c++1z -o build/rwc.out src/main.cpp src/Lexer.cpp
 else
-    echo
-    echo ------------ Building release ------------
-    echo
-    clang++ -O3 -o build/rwc.out src/main.cpp src/Lexer.cpp
-    exit $?
+    echo Building using this script on the current operating system is currently unsupported.
+    echo If you are on windows, then use build.bat or run.bat instead.
 fi
